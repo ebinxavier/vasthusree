503e87a969deffca5be6c8f9c0d536af058207e6
Ebin Xavier <ebin@epidemist.local>
2021-02-10 16:26:36 +0530

    Updates

---
+++ precache-manifest.7fb677627da4cc41d42ab80edf343e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f43333d930db998166a005197d38a94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3294174330b354b75cb9",
+    "url": "/static/css/2.ada51479.chunk.css"
+  },
+  {
+    "revision": "f3aeb4c5b68b71c4ed95",
+    "url": "/static/css/main.98178f2b.chunk.css"
+  },
+  {
+    "revision": "3294174330b354b75cb9",
+    "url": "/static/js/2.aea1571c.chunk.js"
+  },
+  {
+    "revision": "8c148efb6436185179e1b7ec9806bfdd",
+    "url": "/static/js/2.aea1571c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3aeb4c5b68b71c4ed95",
+    "url": "/static/js/main.396a870f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
